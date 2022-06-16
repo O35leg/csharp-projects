463467b08f7618e2c8e0a83c5db3a24c8061e4ba
Oleg Oleg <o2579leg@gmail.com>
2022-06-16 16:00:59 -0400

    init commit

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.WriteLine("Please input different types of shapes. U can pick from: Triangle , Circle, Square, Rectagle.");
+                string shapeSelector =  Console.ReadLine();
+
+                if(shapeSelector == "Square")
+                {
+                    Console.WriteLine("\n|------|");
+                    Console.WriteLine("|      |");
+                    Console.WriteLine("|      |");
+                     Console.WriteLine("-------");
+                }
+
+                if(shapeSelector == "Circle")
+                {
+                    Console.WriteLine("\n() ");
+                }
+
+                if (shapeSelector == "Triangle")
+                {
+                    Console.WriteLine("/|");
+                   Console.WriteLine("/ |");
+                  Console.WriteLine("/  |");
+                 Console.WriteLine("/   |");
+                Console.WriteLine(" -----");
+                }
+
+                if(shapeSelector == "Rectangle")
+                {
+                    Console.WriteLine("|--------|");
+                    Console.WriteLine("|________|");
+                }
+
+                else if(shapeSelector != "Rectangle" && shapeSelector != "Triangle" && shapeSelector != "Circle" && shapeSelector != "Square")
+                {
+                    Console.WriteLine("\nSyntax error: No shape like this in the system. Try agian");
+                }
+            }
+        }
+    }
+}
